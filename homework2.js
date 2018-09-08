f494343a067e864cd25dd06d8a7de6a311367b88
Elen <elen_pluzyan@edu.aua.am>
2018-09-08 13:19:14 +0400

    first commit

---
+++ homework2.js
@@ -0,0 +1,20 @@
+alert('Hi'+' CS nn')
+const qwe ='my answer is'+ (5*9*8/6-9) 
+alert(qwe)
+const a = 8;
+const b = 16;
+const c = b/a;
+
+if(!false===!!!true && a!==8*2) {
+	console.log('false')
+	console.log('trt again')
+} else {
+	console.log('great!')
+}
+if(a===c || b>=9) {
+	console.log('good job')
+}
+else {
+	console.log('need more practice in math')
+}
+console.log('Bye, see u soon!')
